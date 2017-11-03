6abf93c4da320ae3dce5db882f8e136f8937c00e
Dankers Wouter <wouter.dankers@skynet.be>
2017-11-03 19:59:23 +0100

    lane change, lane follow, ultrasound

---
+++ RobotHwDrivers/LaneChangeLaneFollow/controller.h
@@ -0,0 +1,70 @@
+#ifndef _CONTROLLER_H_
+#define _CONTROLLER_H_
+#include <Arduino.h>
+//#define abs(x) (((x)<0) ? -(x) : (x))
+
+typedef struct pos_struct{
+    double x;
+    double y;
+    double theta;
+} pos_t;
+
+typedef struct input_waypoint{
+  int x;
+  int y;
+} input_waypoint_t;
+
+typedef struct encoder {
+    double right;
+    double left;
+
+    struct encoder operator+(struct encoder other){
+        other.right += right;
+        other.left  += left;
+        return other;
+    }
+
+    struct encoder operator-(struct encoder other){
+        return {right-other.right,left-other.left};
+    }
+} encoder_t;
+ 
+class Controller {
+
+public:
+    static constexpr double WHEEL_BASE      = 18; //cm
+    static constexpr double WHEEL_DIAMETER  = 6.5 ; //cm
+    static constexpr double ERESOL          = 192;
+    static constexpr double K1              = 0;
+    static constexpr double K2              = 2;
+    static const     int    ENCODER_MAX     = 0X7ff;
+    
+    double vel_lin = 0;
+    double vel_max = 2;
+    static const int CHANNEL = 1;
+    pos_t waypoint;
+    
+    Controller(pos_t start = {0}, encoder_t encoders = {0});
+    void updatePosition(encoder_t encoder_deltas);
+    void updateEncoders(encoder_t encoder_deltas);
+    encoder_t update(encoder_t encoder_new);
+    void setWaypoint(input_waypoint_t i);
+
+private:
+    pos_t position;
+    encoder_t encoders;
+    encoder_t reading;
+    encoder_t velocity;
+        
+    float Ielines[3] = {0}; 
+    double ego_cosys_ang = 0;
+    double ego_delta = 0;
+    double ego_theta = 0;
+    double kappa = 0;
+    double omega = 0;
+    double d_x = 0;
+    double d_y = 0;
+    double r = 0;
+};
+#endif
+
