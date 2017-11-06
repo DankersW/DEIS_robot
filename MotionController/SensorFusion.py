a8eacc166c1d23021a7a25d9c384117c18354b55
Unknown <dvaritimidis@gmail.com>
2017-11-06 14:48:40 +0100

    Sensor Fusion update

--- MotionController/SensorFusion.py
+++ MotionController/SensorFusion.py
@@ -1,17 +1,18 @@
 
 class SensorFusion:
-    def __init__(self,gps_data,odometry_data):
+    def __init__(self,gps_data,odometry_data,K=0.9):
         self.gps_data=gps_data
         self.odometry_data=odometry_data
+        self.K=K
 
 
     def fusion(self):
         if self.gps_data:
             #print self.gps_data
 
-            estimated_x = 0.9 * self.gps_data[0] + 0.1 * self.odometry_data[0]
-            estimated_y = 0.9 * self.gps_data[1] + 0.1 * self.odometry_data[1]
-            estimated_theta = 0.9 * self.gps_theta[2] + 0.1 * self.odometry_data[2]
+            estimated_x = self.K * self.gps_data[0] + (1-self.K) * self.odometry_data[0]
+            estimated_y = self.K * self.gps_data[1] + (1-self.K) * self.odometry_data[1]
+            estimated_theta = self.K * self.gps_theta[2] + (1-self.K) * self.odometry_data[2]
 
             estimated_pos=[estimated_x,estimated_y,estimated_theta]
             print estimated_pos
