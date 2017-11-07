c7f4dae256a9cf20043a768a386566ea488fa2c3
Unknown <dvaritimidis@gmail.com>
2017-11-07 11:57:51 +0100

    Kalman filter variable names changed

--- MotionController/KalmanFilter.py
+++ MotionController/KalmanFilter.py
@@ -2,20 +2,20 @@ import numpy as np
 
 class KalmanFilter(object):
 
-    def __init__(self, process_variance, estimated_measurement_variance):
-        self.process_variance = process_variance # Q
-        self.estimated_measurement_variance = estimated_measurement_variance # R
-        self.posteri_estimate = 0.0 # Xhat
-        self.posteri_error_estimate = 1.0 # P
+    def __init__(self,odometry_data, process_variance=1e-5, estimated_measurement_variance=0.1**2):
+        self.Q = process_variance # Q process_variance
+        self.R = estimated_measurement_variance # R estimated_measurement_variance
+        self.Xhat = odometry_data # Xhat estimated_measurement_variance
+        self.P = 1.0 # P posteri_error_estimate
 
-    def input_latest_sensor_measurement(self, measurement):
-        self.measurement_z=np.array(measurement) # z
-        priori_estimate = self.posteri_estimate # xhatminus[k] = xhat[k-1]
-        priori_error_estimate = self.posteri_error_estimate + self.process_variance #    Pminus[k] = P[k-1]+Q
+    def input_latest_sensor_measurement(self, measurement): # GPS measurment
+        self.z=np.array(measurement) # z
+        Xhatminus = self.Xhat # priori_estimate = self.posteri_estimate xhatminus[k] = xhat[k-1]
+        Pminus = self.P + self.Q #priori_error_estimate = posteri_error_estimate + process_variance     Pminus[k] = P[k-1]+Q
 
-        blending_factor = priori_error_estimate / (priori_error_estimate + self.estimated_measurement_variance) # K[k] = Pminus[k]/( Pminus[k]+R )
-        self.posteri_estimate = priori_estimate + blending_factor * (self.measurement_z - priori_estimate) # xhat[k] = xhatminus[k]+K[k]*(z[k]-xhatminus[k])
-        self.posteri_error_estimate = (1 - blending_factor) * priori_error_estimate # P[k] = (1-K[k])*Pminus[k]
+        K = Pminus / (Pminus + self.R) # blending_factor = priori_error_estimate / (priori_error_estimate + estimated_measurement_variance) # K[k] = Pminus[k]/( Pminus[k]+R )
+        self.Xhat = Xhatminus + K * (self.z - Xhatminus) # posteri_estimate = priori_estimate + blending_factor * (measurement_z - priori_estimate) \\ xhat[k] = xhatminus[k]+K[k]*(z[k]-xhatminus[k])
+        self.P = (1 - K) * Pminus # posteri_error_estimate = (1 - blending_factor) * priori_error_estimate \\ P[k] = (1-K[k])*Pminus[k]
 
     def get_latest_estimated_measurement(self):
-        return self.posteri_estimate # Xhat
+        return self.Xhat # Xhat posteri_estimate
