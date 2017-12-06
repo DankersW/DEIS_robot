1779c3139e7216433ba4d1e673a0bdb8dddf3ecd
Unknown <alegal16@student.hh.se>
2017-12-06 13:54:38 +0100

    Included Switch to specific lane

--- LaneDetection/TurnCheck.py
+++ LaneDetection/TurnCheck.py
@@ -8,9 +8,11 @@ class TurnCheck(object):
 
 
     def __init__(self,imsize,LookUpTableImage):
+
         self.LookUpTable = cv2.imread(LookUpTableImage,cv2.IMREAD_GRAYSCALE)
         self.DirectionLookUpTable = np.zeros((imsize[1],imsize[0]),dtype=np.uint8)
-
+        self.cw = False
+        self.lane = 1
         for y in range(imsize[0]):
             for x in range (imsize[1]):
                 if y >= imsize[0]/2:
@@ -24,46 +26,64 @@ class TurnCheck(object):
                     else:
                         self.DirectionLookUpTable[x,y] = 2
 
-        #cv2.imshow('Direction',self.DirectionLookUpTable*50)
-        #cv2.waitKey(0)
 
+    def get_direction(self):
+        return self.cw
 
-
-    def GetLane(self,coord_x,coord_y,orientation):
+    def compute_direction(self,coord_x,coord_y,orientation):
         orient = (round(2*orientation/math.pi) * math.pi/2)
         print orient
-        cw = False
         if(orientation == math.pi or orientation == -math.pi):
             if(self.DirectionLookUpTable[coord_x,coord_y] == 2 or self.DirectionLookUpTable[coord_x,coord_y] == 3):
-                cw = True
+                self.cw = True
         elif(orientation == math.pi/2):
             if (self.DirectionLookUpTable[coord_x, coord_y] == 1 or self.DirectionLookUpTable[coord_x, coord_y] == 2):
-                cw = True
+               self.cw = True
         elif(orientation == -math.pi/2):
             if (self.DirectionLookUpTable[coord_x, coord_y] == 4 or self.DirectionLookUpTable[coord_x, coord_y] == 3):
-                cw = True
+                self.cw = True
         else:
             if (self.DirectionLookUpTable[coord_x, coord_y] == 1 or self.DirectionLookUpTable[coord_x, coord_y] == 4):
-                cw = True
+                self.cw = True
 
-        Lane = self.LookUpTable[coord_x][coord_y]
-        if cw:
-            print 'CW'
-            if Lane == 1 :
-                return [Lane, 1] # right
-            elif Lane == 4:
-                return [Lane, 0] # left
+    def get_lane(self,coord_x,coord_y):
+
+        self.lane = self.LookUpTable[coord_x][coord_y]
+
+        if self.cw:
+            if self.lane == 1:
+                return [self.lane, 1] # right
+            elif self.lane == 4:
+                return [self.lane, 0] # left
             else:
-                return [Lane, random.randint(0,1)] #random
+                return [self.lane, random.randint(0,1)] #random
 
         else:
-            print 'CCW'
-            if Lane == 1:
-                return [Lane, 0]
-            elif Lane == 4:
-                return [Lane, 1]
+            if self.lane == 1:
+                return [self.lane, 0]
+            elif self.lane == 4:
+                return [self.lane, 1]
             else:
-                return [Lane, random.randint(0,1)]
+                return [self.lane, random.randint(0,1)]
+
+    def get_lane_switch_commands(self,Lane_to_change_to):
+
+        lanechange = self.lane - Lane_to_change_to
+
+        if(lanechange<0):
+            if self.cw:
+                return [1,abs(lanechange)] # right
+            else:
+                return [0,abs(lanechange)] # left
+        elif(lanechange>0):
+            if self.cw:
+                return [0,abs(lanechange)]
+            else:
+                return [1,abs(lanechange)]
+        else:
+            return [-1,-1] # no change we are already here
+
+
 
 
 
